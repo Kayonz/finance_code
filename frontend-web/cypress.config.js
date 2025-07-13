f60e679b6109b97dc425edbf177598f77b9773c8
naruto <vitorlara261@gmail.com>
2025-07-13 17:23:17 -0300

    Feature: arrumando codigo e testes

--- frontend-web/cypress.config.js
+++ frontend-web/cypress.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from 'cypress';
+const { defineConfig } = require('cypress');
 
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
@@ -9,4 +9,3 @@ export default defineConfig({
     supportFile: false,
   },
 });
-
